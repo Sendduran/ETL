bc3783e6b141f25f52612a7776eaac5a70925401
Sendduran Thailan <dsendduran@gmail.com>
2025-06-07 08:01:46 -0400

    Created a model for facts table

--- etl_app/main.py
+++ etl_app/main.py
@@ -1,14 +1,33 @@
 from etl_app.tools import scrape
-from etl_app.connection import Connection
-from .sentry_config import init_sentry
+from sentry_config import init_sentry
+
+from sqlmodel import SQLModel
+from etl_app.db_models import facts_model 
+from etl_app.connection import engine, SQLModel
+#Always import the models and the connection. SQLModel (table = true) will create a metadata . without the metadata createall doest work.
 
 
 init_sentry()
 
-a = 1/0
+
+############################## Do not delete  ########################################################
+
+# striptime = datetime.strptime("2023-10-02T02:22:00.272Z", "%Y-%m-%dT%H:%M:%S.%fZ")
+# print((striptime.timestamp()))
+# dt = datetime.now()
+# text = dt.isoformat()
+# print(text)
+# striptime = datetime.strptime(text,"%Y-%m-%dT%H:%M:%S.%f")
+# print({":,.6f"}.format(float(striptime.timestamp())))
+# print(type(text))
+#######################################################################################################################
+
 
 def main():
-    data:str = scrape()
-    return data
+    SQLModel.metadata.create_all(engine)
+    # data:str = scrape()
+    return None
 
 main()
+
+
