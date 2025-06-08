c51f3f2ce54283a2c1a80969206e7f97fc94f6dc
Sendduran Thailan <dsendduran@gmail.com>
2025-06-08 18:57:15 -0400

    database populating code is added

--- etl_app/main.py
+++ etl_app/main.py
@@ -2,13 +2,6 @@ from etl_app.tools import scrape , bulk_insert_fact
 from sentry_config import init_sentry
 from etl_app.connection import DatabaseConnection
 
-############################## Do not delete  ########################################################
-
-
-#######################################################################################################################
-
-
-
 db_name = "facts_db"
 db_user = "postgres"
 db_password = "admin"
@@ -16,14 +9,11 @@ db_server = "localhost"
 db_port = 5432
 
 def main():    
-    # init_sentry()
-    # # scrape()
+    init_sentry()
+    facts_data =  scrape()
     db = DatabaseConnection(db_name, db_user, db_password, db_server, db_port)
-    db.create_db_and_tables()
-    facts_data = [{"fact": "Heres looking at you. Dogs have three eyelids, an upper lid, a lower lid and the third lid, called a nictitating membrane or \"haw,\" which helps keep the eye moist and protected.",
-    "created_date": "2023-10-02T02:22:00.272Z"},{"fact": "Dogs are direct descendants of wolves.", "created_date": "2024-05-12T08:17:49.657Z"}]
+    db.create_db_and_tables()    
     bulk_insert_fact(facts_data , db.get_session())
     
-
 if __name__ == "__main__":
     main()
--- etl_app/tools/tools.py
+++ etl_app/tools/tools.py
@@ -1,15 +1,10 @@
 import json
-import re
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from etl_app.db_models.facts_model import Facts
 from  etl_app.connection import DatabaseConnection
 from sqlmodel import Session
 from datetime import datetime
-
-current_dir:str = os.path.dirname(__file__)
-file_path: str = os.path.join(current_dir, '../data/sample.json')
 
 def clean_unicode(uncleaned_strting: str) -> str:    
     try:
@@ -18,39 +13,25 @@ def clean_unicode(uncleaned_strting: str) -> str:
         pass
     return cleaned_string
 
-# def add_category(data_list:list[dict])->list[dict]:
-#     for item in data_list:
-#         is_con_no:bool = True if re.search(r"\d+",item["fact"]) else False
-#         item.update({"has_no":is_con_no})
-#     return data_list
-
-# # Checks for number in the given string and returns a bool
-# def check_for_no(fact:str)-> bool:
-#     has_no:bool  = True if re.search(r"\d+", fact) else False
-#     return has_no
-
 #converts the given the string to timestamp
 def convert_to_timestamp(time:str)-> int:
     striptime: str  = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%fZ")
     return int(float(striptime.timestamp())*1000000)
 
-def scrape():
+def scrape()-> list[dict]:
     driver=webdriver.Firefox()
     driver.get("https://raw.githubusercontent.com/vetstoria/random-k9-etl/refs/heads/main/source_data.json")
     elem  = driver.find_element(By.TAG_NAME, "body")
     body:str = elem.get_attribute('innerText')
     json_data:list[dict] = json.loads(body)
-    cleaned_data:list[dict] = clean_unicode(json_data)
     driver.close()
-    return cleaned_data
+    return json_data
 
-def bulk_insert_fact(facts_data:list[dict] , session:Session):
-    facts = []
+def bulk_insert_fact(facts_data:list[dict] , session:Session) -> dict:
     for item in facts_data:
-        item.update(fact = clean_unicode(item['fact']))
-        item.update({"facts_id": convert_to_timestamp(item['created_date'])})
-        # print(item)
-        facts.append(item)  
-    # print(facts)
-    session.bulk_insert_mappings(Facts, facts)
-    session.commit()
+        item.update(fact = clean_unicode(item['fact'])) # update the dictionary with clened fact
+        item.update({"facts_id": convert_to_timestamp(item['created_date'])})     # create a new key "facts_id" based on the created_date  
+    session.bulk_insert_mappings(Facts, facts_data) # sqlalchemy module to input multiple data at once
+    session.commit() #commit the data to the database
+    session.close() # close the session
+    return {"message": "Data added to the database successfully"}
