79681284ba73ec81c3a91a19101e2f1ad8a6b2a9
Murshid Haniffa <murshidhaniffa@gmail.com>
2025-06-09 13:04:30 +0530

    minor update - 1

--- etl_app/main.py
+++ etl_app/main.py
@@ -1,19 +1,21 @@
 from etl_app.tools import scrape , bulk_insert_fact
 from sentry_config import init_sentry
 from etl_app.connection import DatabaseConnection
+from dotenv import load_dotenv
+import os
 
-db_name = "facts_db"
-db_user = "postgres"
-db_password = "admin"
-db_server = "localhost"
-db_port = 5432
+db_name = os.getenv("db_name")
+db_user = os.getenv("db_user")
+db_password = os.getenv("db_password")
+db_server = os.getenv("db_server")
+db_port = os.getenv("db_port")
 
-def main():    
+def main():
     init_sentry()
     facts_data =  scrape()
     db = DatabaseConnection(db_name, db_user, db_password, db_server, db_port)
-    db.create_db_and_tables()    
+    db.create_db_and_tables()
     bulk_insert_fact(facts_data , db.get_session())
-    
+
 if __name__ == "__main__":
     main()
